8301cb42b631e562995e8832bd95e4a84be50c32
Marchal yoan <marchalyoan@gmail.com>
2016-04-21 12:08:45 -0400

    Applied fixes from StyleCI

--- functions.php
+++ functions.php
@@ -3,113 +3,103 @@
  * Custom template tags for this theme.
  *
  * Eventually, some of the functionality here could be replaced by core features.
- *
- * @package AME2016
- * https://amged.me/
  */
-
-if ( ! function_exists( 'build_jsonld' ) ) :
+if (!function_exists('build_jsonld')) :
 /*
 * build json-ld for SEO
 */
-function build_jsonld($return=false){
-	require_once(get_template_directory() . '/jsonld.php');
-	$output = jsonLD();  
-	if ($return)
-	{
-		return json_encode($output);
-	}
-	else
-	{
-		echo '<script type="application/ld+json">' . json_encode($output) . '</script>';
-	}
+function build_jsonld($return = false)
+{
+    require_once get_template_directory().'/jsonld.php';
+    $output = jsonLD();
+    if ($return) {
+        return json_encode($output);
+    } else {
+        echo '<script type="application/ld+json">'.json_encode($output).'</script>';
+    }
 }
 endif;
-add_action( 'wp_head', 'build_jsonld' );
+add_action('wp_head', 'build_jsonld');
 
-if ( ! function_exists( 'build_jsonldBreadcrumb' ) ) :
+if (!function_exists('build_jsonldBreadcrumb')) :
 
 /*
 build json-ld for SEO items list
 https://amged.me/
 */
-function build_jsonldBreadcrumb(){
-	$jsonLD["@context"] = "http://schema.org/";
-	$jsonLD["@type"] = "BreadcrumbList";
-	//---------------------------------
-	// pre-define some variables
-	//---------------------------------
-	$home_title = get_bloginfo('name');
-	$home_url = esc_url( home_url( '/' ) );
-	$blog_url = get_permalink( get_option( 'page_for_posts' ) );
-	$blog_title = get_the_title( get_option('page_for_posts', true) );
-	$url = $url ? $url : get_permalink();
-	$title = $title ? $title : get_the_title();
-	$itemList = array();
+function build_jsonldBreadcrumb()
+{
+    $jsonLD['@context'] = 'http://schema.org/';
+    $jsonLD['@type'] = 'BreadcrumbList';
+    //---------------------------------
+    // pre-define some variables
+    //---------------------------------
+    $home_title = get_bloginfo('name');
+    $home_url = esc_url(home_url('/'));
+    $blog_url = get_permalink(get_option('page_for_posts'));
+    $blog_title = get_the_title(get_option('page_for_posts', true));
+    $url = $url ? $url : get_permalink();
+    $title = $title ? $title : get_the_title();
+    $itemList = [];
 
-	//----------------------------------
-	// Output
-	//-----------------------------------
-	 $itemList[] = array(
-						  "@type" => "ListItem",
-						  "position" => 1,
-						  "item" => array('@id' => $home_url ,'name' => $home_title),
-					  );
-	//----------------------------------------------
-	// figure out the next number
-	// <meta property="position" content="{NUM}">
-	//----------------------------------------------
-	$next = 2;
-	if ( is_single() or is_home())
-	{
-		//-------------------------------------
-		// we have a blog "post" 
-		// so we added the blog url (not home)
-		// so next is 3 not 2!
-		//--------------------------------------
-		$itemList[] = array(
-						  "@type" => "ListItem",
-						  "position" => 2,
-						  "item" => array('@id' => $blog_url ,'name' => $blog_title),
-					  );
-		$next = 3;
-	}
-	if ( is_single() or is_page() or is_tag() or is_category()) {
-	
-	//----------------------------------
-	// is it a tag?
-	//----------------------------------
-	if ( is_tag() )
-	{
-		$title  = single_tag_title("", false);
-		$tag_id = get_term_by('name', $title, 'post_tag');
-		$url    = get_tag_link($tag_id->term_id);
-	}
-	
-	if ( is_category() )
-	{
-		$title  = single_cat_title("", false);
-		$category_id = get_cat_ID( $title );
-        $url = get_category_link( $category_id );
-	}
-	
-	$itemList[] = array(
-						  "@type" => "ListItem",
-						  "position" => $next,
-						  "item" => array('@id' => $url, 'name' => $title),
-					  );
-					  
-	}
-	$jsonLD["itemListElement"] = $itemList;
-	/*echo '<pre>';
-	print_r(json_encode($jsonLD));
-	echo '</pre>';*/
-	if ( (is_page() OR is_single() or is_home() or is_tag() or is_category()) AND (! is_front_page()) ) {
-	echo '<script type="application/ld+json">';
-	echo json_encode($jsonLD);
-	echo '</script>';
-	}
+    //----------------------------------
+    // Output
+    //-----------------------------------
+     $itemList[] = [
+                          '@type'    => 'ListItem',
+                          'position' => 1,
+                          'item'     => ['@id' => $home_url, 'name' => $home_title],
+                      ];
+    //----------------------------------------------
+    // figure out the next number
+    // <meta property="position" content="{NUM}">
+    //----------------------------------------------
+    $next = 2;
+    if (is_single() or is_home()) {
+        //-------------------------------------
+        // we have a blog "post"
+        // so we added the blog url (not home)
+        // so next is 3 not 2!
+        //--------------------------------------
+        $itemList[] = [
+                          '@type'    => 'ListItem',
+                          'position' => 2,
+                          'item'     => ['@id' => $blog_url, 'name' => $blog_title],
+                      ];
+        $next = 3;
+    }
+    if (is_single() or is_page() or is_tag() or is_category()) {
 
+    //----------------------------------
+    // is it a tag?
+    //----------------------------------
+    if (is_tag()) {
+        $title = single_tag_title('', false);
+        $tag_id = get_term_by('name', $title, 'post_tag');
+        $url = get_tag_link($tag_id->term_id);
+    }
+
+        if (is_category()) {
+            $title = single_cat_title('', false);
+            $category_id = get_cat_ID($title);
+            $url = get_category_link($category_id);
+        }
+
+        $itemList[] = [
+                          '@type'    => 'ListItem',
+                          'position' => $next,
+                          'item'     => ['@id' => $url, 'name' => $title],
+                      ];
+    }
+    $jsonLD['itemListElement'] = $itemList;
+    /*echo '<pre>';
+    print_r(json_encode($jsonLD));
+    echo '</pre>';*/
+    if ((is_page() or is_single() or is_home() or is_tag() or is_category()) and (!is_front_page())) {
+        echo '<script type="application/ld+json">';
+        echo json_encode($jsonLD);
+        echo '</script>';
+    }
 }
 endif;
-add_action( 'wp_head', 'build_jsonldBreadcrumb' );
+add_action('wp_head', 'build_jsonldBreadcrumb');
--- jsonld.php
+++ jsonld.php
@@ -1,218 +1,206 @@
 <?php
 /**
- JSON-LD Wordpress JSON-LD Generator
- For Homepage, Articles, Author Pages & Breadcrumbs
- @Author: Amged Osman
  @Link: https://amged.me
 */
+function jsonLD()
+{
+    //------------------------
+    // Start
+    //-----------------------
+    $jsonLD['@context'] = 'http://schema.org/';
 
+    //--------------------------------------------
+    // get post/page data
+    //--------------------------------------------
+    $postData = get_post_data();
 
-function jsonLD(){
-	//------------------------
-	// Start
-	//-----------------------
-	$jsonLD["@context"] = "http://schema.org/";
+    //--------------------------------------------
+    // Get Single Tag
+    // or category
+    //--------------------------------------------
+    if (($tags = wp_get_post_tags($postData->ID)) != null) {
+        $articleSection = $tags[0]->name;
+    } else {
+        $category = get_the_category();
+        $articleSection = $category[0]->cat_name;
+    }
 
-	//--------------------------------------------
-	// get post/page data
-	//--------------------------------------------
-	$postData = get_post_data();
-	
-	//--------------------------------------------
-	// Get Single Tag
-	// or category
-	//--------------------------------------------
-	if ( ( $tags = wp_get_post_tags($postData->ID) ) != null)
-	{
-		$articleSection = $tags[0]->name;
-	} else {
-		$category = get_the_category();
-		$articleSection = $category[0]->cat_name;
-	}
-	
-	//--------------------------------------------
-	//common
-	//--------------------------------------------
-	$home_title = get_bloginfo('name');
-	$home_url   = esc_url( home_url( '/' ) );
-	$blog_url   = get_permalink( get_option( 'page_for_posts' ) );
-	$blog_title = get_the_title( get_option('page_for_posts', true) );
-	
+    //--------------------------------------------
+    //common
+    //--------------------------------------------
+    $home_title = get_bloginfo('name');
+    $home_url = esc_url(home_url('/'));
+    $blog_url = get_permalink(get_option('page_for_posts'));
+    $blog_title = get_the_title(get_option('page_for_posts', true));
 
-	//--------------------------------------------
-	// Is is a post?
-	//--------------------------------------------
-	if (is_single()) {
-	 
-	  $authorData = get_userdata($postData->post_author);
-	  $postUrl = get_permalink();
-	  $postPhoto = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
-	  //$postPhoto = get_featured_url('image-1x1');
+    //--------------------------------------------
+    // Is is a post?
+    //--------------------------------------------
+    if (is_single()) {
+        $authorData = get_userdata($postData->post_author);
+        $postUrl = get_permalink();
+        $postPhoto = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
+      //$postPhoto = get_featured_url('image-1x1');
 
-	  $jsonLD["@type"] = "Article";
-	  $jsonLD["url"] = $postUrl;
-	  $jsonLD["author"] = array(
-		  "@type" => "Person",
-		  "name" => $authorData->display_name,
-		  );
-		  
-	  $jsonLD["headline"] = $postData->post_title;
-	  
-	  $jsonLD["datePublished"] = $postData->post_date;
-	  
-	  // get the tags if not get the category if not don't even display it!
-	  if ( $articleSection !== null )
-	  {
-		$jsonLD["ArticleSection"] = $articleSection;
-	  }
-	  
-	  // do we have description?
-	  if ($postData->post_excerpt != '') 
-	  {
-		$jsonLD["description"] = $postData->post_excerpt;
-	  }
-	  // photo?
-	  if ( $postPhoto )
-	  {
-		$jsonLD["image"] = $postPhoto;
-	  }
-	  
-	  $jsonLD["Publisher"] = $home_title;
-	}
+      $jsonLD['@type'] = 'Article';
+        $jsonLD['url'] = $postUrl;
+        $jsonLD['author'] = [
+          '@type' => 'Person',
+          'name'  => $authorData->display_name,
+          ];
 
-	//---------------------------
-	// @type: Organization
-	// set it up manually!!!
-	//----------------------------
+        $jsonLD['headline'] = $postData->post_title;
 
-	if (is_front_page()) {
-	  $jsonLD["@type"] = "Organization";
-	  $jsonLD["name"] = $home_title;
-	  //------------------------------
-	  // You can add different name
-	  //-------------------------------
-	  $jsonLD["alternateName"] = "أمجد عثمان";
-	  $jsonLD["logo"] = "https://domain.me/cdn/images/static/logo_a-lato2_300x248.png";
-	  $jsonLD["url"] = $home_url;
-	  $jsonLD["sameAs"] = array(
-		"https://twitter.com/username",
-		"https://www.facebook.com/username",
-		"https://www.linkedin.com/in/username",
-		"https://instagram.com/username/",
-		"https://soundcloud.com/username",
-		"https://www.pinterest.com/username/",
-		"https://plus.google.com/+username/",
-		"https://www.youtube.com/user/username",
-		
-	  );
+        $jsonLD['datePublished'] = $postData->post_date;
 
-	//--------------------------------
-	// for organizations only
-	// they can add their contact info
-	//----------------------------------
-	  $jsonLD["contactPoint"] = array(
-		array(
-		  "@type" => "ContactPoint",
-		  "telephone" => "+966 500 000 000",
-		  "email" => "user@domain.me",
-		  "contactType" => "sales",
-		  "availableLanguage" => array(
-											"English",
-											"Arabic",
-										  )
-		)
-	  );
-	  //------------------------------------
-	  // setup search
-	  // read here
-	  // https://developers.google.com/structured-data/slsb-overview
-	  //-----------------------------------
-	  $jsonLD["potentialAction"] = array(
-		array(
-		  "@type" => "SearchAction",
-		  "target" => "https://domain.com/?s={searchTerm}",
-		  "query-input" => "required name=searchTerm"
-		)
-	  );
-	  
-	}
+      // get the tags if not get the category if not don't even display it!
+      if ($articleSection !== null) {
+          $jsonLD['ArticleSection'] = $articleSection;
+      }
 
-	if (is_author()) {
-	  //------------------------------
-	  // get us some authorData
-	  //------------------------------
-	  $authorData = get_userdata($postData->post_author);
-	  
-	  //---------------------------------------
-	  // set up all networks you want to pull
-	  // you don't need to worry if the author
-	  // filled it or not
-	  // because we're performing a checkup
-	  // the "networks" fields are custom field
-	  // read here http://davidwalsh.name/add-profile-fields
-	  // @usage
-	  // $metas = array('facebook', 'googleplus');
-	  //----------------------------------------
-	  $metas = array('twitter', 'url', 'facebook', 'googleplus', 'linkedin', 'soundcloud', 'tumblr');
-	  $sameAs = array();
-	  //------------------------------------------------------
-	  // append Twitter Url
-	  // because davidwalsh created it for the handle only :D!
-	  //-------------------------------------------------------
-	  $twitterUrl =  "https://twitter.com/";
-	  foreach ($metas as $meta)
-	  {
-		if ( get_the_author_meta($meta) != '' )
-		{
-			$network = get_the_author_meta($meta);
-			if ($meta == 'twitter')
-			{
-				$network = $twitterUrl . get_the_author_meta($meta);
-			}
-			$sameAs[] = $network;
-		}
-		
-	  }
-	//----------------------------------
-	// now add more author info
-	// the "jobTitle" field is custom field
-	// read here http://davidwalsh.name/add-profile-fields
-	//----------------------------------
-	  $jsonLD["@type"] = "Person";
-	  $jsonLD["name"] = $authorData->display_name;
-	  $jsonLD["email"] = $authorData->user_email;
-	  if ( get_the_author_meta('title') != '')
-	  {
-		$jsonLD["jobTitle"] = get_the_author_meta('title');
-	  }
-	  $jsonLD["sameAs"] = $sameAs;
-	}
-	return  $jsonLD;
-}
+      // do we have description?
+      if ($postData->post_excerpt != '') {
+          $jsonLD['description'] = $postData->post_excerpt;
+      }
+      // photo?
+      if ($postPhoto) {
+          $jsonLD['image'] = $postPhoto;
+      }
 
+        $jsonLD['Publisher'] = $home_title;
+    }
 
-/* 
-	get some post data
-	not sure if we even need it
-	@return array();
-*/
-function get_post_data() {
-  global $post;
-  return $post;
+    //---------------------------
+    // @type: Organization
+    // set it up manually!!!
+    //----------------------------
+
+    if (is_front_page()) {
+        $jsonLD['@type'] = 'Organization';
+        $jsonLD['name'] = $home_title;
+      //------------------------------
+      // You can add different name
+      //-------------------------------
+      $jsonLD['alternateName'] = 'أمجد عثمان';
+        $jsonLD['logo'] = 'https://domain.me/cdn/images/static/logo_a-lato2_300x248.png';
+        $jsonLD['url'] = $home_url;
+        $jsonLD['sameAs'] = [
+        'https://twitter.com/username',
+        'https://www.facebook.com/username',
+        'https://www.linkedin.com/in/username',
+        'https://instagram.com/username/',
+        'https://soundcloud.com/username',
+        'https://www.pinterest.com/username/',
+        'https://plus.google.com/+username/',
+        'https://www.youtube.com/user/username',
+
+      ];
+
+    //--------------------------------
+    // for organizations only
+    // they can add their contact info
+    //----------------------------------
+      $jsonLD['contactPoint'] = [
+        [
+          '@type'             => 'ContactPoint',
+          'telephone'         => '+966 500 000 000',
+          'email'             => 'user@domain.me',
+          'contactType'       => 'sales',
+          'availableLanguage' => [
+                                            'English',
+                                            'Arabic',
+                                          ],
+        ],
+      ];
+      //------------------------------------
+      // setup search
+      // read here
+      // https://developers.google.com/structured-data/slsb-overview
+      //-----------------------------------
+      $jsonLD['potentialAction'] = [
+        [
+          '@type'       => 'SearchAction',
+          'target'      => 'https://domain.com/?s={searchTerm}',
+          'query-input' => 'required name=searchTerm',
+        ],
+      ];
+    }
+
+    if (is_author()) {
+        //------------------------------
+      // get us some authorData
+      //------------------------------
+      $authorData = get_userdata($postData->post_author);
+
+      //---------------------------------------
+      // set up all networks you want to pull
+      // you don't need to worry if the author
+      // filled it or not
+      // because we're performing a checkup
+      // the "networks" fields are custom field
+      // read here http://davidwalsh.name/add-profile-fields
+      // @usage
+      // $metas = array('facebook', 'googleplus');
+      //----------------------------------------
+      $metas = ['twitter', 'url', 'facebook', 'googleplus', 'linkedin', 'soundcloud', 'tumblr'];
+        $sameAs = [];
+      //------------------------------------------------------
+      // append Twitter Url
+      // because davidwalsh created it for the handle only :D!
+      //-------------------------------------------------------
+      $twitterUrl = 'https://twitter.com/';
+        foreach ($metas as $meta) {
+            if (get_the_author_meta($meta) != '') {
+                $network = get_the_author_meta($meta);
+                if ($meta == 'twitter') {
+                    $network = $twitterUrl.get_the_author_meta($meta);
+                }
+                $sameAs[] = $network;
+            }
+        }
+    //----------------------------------
+    // now add more author info
+    // the "jobTitle" field is custom field
+    // read here http://davidwalsh.name/add-profile-fields
+    //----------------------------------
+      $jsonLD['@type'] = 'Person';
+        $jsonLD['name'] = $authorData->display_name;
+        $jsonLD['email'] = $authorData->user_email;
+        if (get_the_author_meta('title') != '') {
+            $jsonLD['jobTitle'] = get_the_author_meta('title');
+        }
+        $jsonLD['sameAs'] = $sameAs;
+    }
+
+    return  $jsonLD;
 }
 
 /*
-// get get_featured_url 
+    get some post data
+    not sure if we even need it
+    @return array();
+*/
+function get_post_data()
+{
+    global $post;
+
+    return $post;
+}
+
+/*
+// get get_featured_url
 // In your template ...
 // Get src URL from avatar <img> tag (add to functions.php)
 // this will also grab a default image if the post has no attachments
 // @Usage get_featured_url('thumb-small');
 */
-function get_featured_url($size="full"){
-    if ( has_post_thumbnail()) {
-     $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), $size);
-     return( $large_image_url[0]);
+function get_featured_url($size = 'full')
+{
+    if (has_post_thumbnail()) {
+        $large_image_url = wp_get_attachment_image_src(get_post_thumbnail_id(), $size);
+
+        return  $large_image_url[0];
     } else {
-		return get_template_directory_uri()."/images/default.png";
+        return get_template_directory_uri().'/images/default.png';
     }
 }
